cf99f813b60130296bdcb9938f8dc4e98c03b5e6
divinirakiza <irakizadivin@gmail.com>
2021-11-16 10:47:31 +0200

    (feat): Secured basic APIS

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,19 +2,20 @@ const jwt = require('jsonwebtoken');
 const { User } = require('../models/user.model');
 const { ERROR_RESPONSE } = require('../utils/common.util');
 
+const SECRET_KEY = process.env.SECRET_KEY;
 
-const  AUTH_MIDDLEWARE = (req, res, next) => {
+const  AUTH_MIDDLEWARE = async (req, res, next) => {
     const header = req.header('Authorization');
 
      if (!header) 
-        return res.status(404).send(ERROR_RESPONSE(null, 'Authorisatino header not provided'));
+        return res.status(404).send(ERROR_RESPONSE(null, 'Authorization Headers not provided'));
 
      if (!(header.startsWith("Bearer ")))
         return res.status(404).send(ERROR_RESPONSE(null, 'Invalid Token Format'));
 
     const token = header.split(' ')[1];
     try {
-        const decoded = jwt.verify(token);
+        const decoded = jwt.verify(token, SECRET_KEY);
         console.log(decoded);
 
         const user = await User.findById(decoded._id);
@@ -29,3 +30,5 @@ const  AUTH_MIDDLEWARE = (req, res, next) => {
     }
 
 }
+
+module.exports = {AUTH_MIDDLEWARE};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const  {getAll, getById, create} = require('../controllers/user.controller');
+const {AUTH_MIDDLEWARE} = require('../middlewares/auth.middleware');
 
-
-router.get('/api/users', getAll);
+router.get('/api/users', AUTH_MIDDLEWARE, getAll);
 router.get('/api/users:id', getById);
 router.post('/api/users', create);
 
